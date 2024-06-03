3a83639078650bcbfa476bf49909feba729640e6
Lehatran <leha.tran277@gmail.com>
2024-06-04 00:47:23 +0700

    LE HA push code borrow return

---
+++ Areas/Admin/Controllers/BorrowAdminController.cs
@@ -0,0 +1,66 @@
+﻿using HTQLTV.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using X.PagedList;
+using Azure;
+
+namespace HTQLTV.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/BorrowAdmin")]
+    public class BorrowAdminController : Controller
+    {
+        HtqltvContext db = new HtqltvContext();
+
+        // GET: Borrow
+        [HttpGet]
+        [Route("ListBorrowReturn")]
+        public IActionResult ListBorrowReturn(int? page)
+        {
+            int pageNumber = page ?? 1;
+            int pageSize = 10;
+            var borrowReturns = db.BorrowReturns.Include(br => br.Book)
+                                                 .Include(br => br.Reader)
+                                                 .Include(br => br.Staff)
+                                                 .ToPagedList(pageNumber, pageSize);
+            return View(borrowReturns);
+        }
+
+        // GET: Borrow/Create
+        [HttpGet]
+        [Route("CreateBorrow")]
+        public IActionResult CreateBorrow()
+        {
+            return View();
+        }
+
+        // POST: Borrow/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateBorrow(BorrowReturn borrowReturn)
+        {
+            if (ModelState.IsValid)
+            {
+                // Giả sử User.Identity.Name là tên đăng nhập của nhân viên hiện tại
+                var username = User.Identity.Name;
+                var currentStaff = db.Staff.FirstOrDefault(s => s.Email == username);
+
+                if (currentStaff != null)
+                {
+                    borrowReturn.StaffId = currentStaff.StaffId;
+                    // Tạo StatId mới hoặc thiết lập giá trị phù hợp
+                    borrowReturn.StatId = Guid.NewGuid().ToString();
+                    db.BorrowReturns.Add(borrowReturn);
+                    db.SaveChanges();
+                    return RedirectToAction("ListBorrowReturn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Không thể xác định nhân viên hiện tại.");
+                }
+            }
+
+            return View(borrowReturn);
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/ReturnAdminController.cs
@@ -0,0 +1,70 @@
+﻿using HTQLTV.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using X.PagedList;
+
+namespace HTQLTV.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/ReturnAdmin")]
+    public class ReturnAdminController : Controller
+    {
+        
+        HtqltvContext db = new HtqltvContext();
+        // GET: Return/ListBorrowReturn
+        [HttpGet]
+        [Route("ListBorrowReturn")]
+        public IActionResult ListBorrowReturn(int? page)
+        {
+            int pageNumber = page ?? 1;
+            int pageSize = 10;
+            var borrowReturns = db.BorrowReturns.Include(br => br.Book)
+                                                 .Include(br => br.Reader)
+                                                 .Include(br => br.Staff)
+                                                 .ToPagedList(pageNumber, pageSize);
+            return View(borrowReturns);
+        }
+
+        // GET: Return/Edit/{id}
+        [HttpGet]
+        [Route("return/{id}")]
+        public IActionResult Return(int id)
+        {
+            var borrowReturn = db.BorrowReturns.Include(br => br.Book)
+                                                .Include(br => br.Reader)
+                                                .Include(br => br.Staff)
+                                                .FirstOrDefault(br => br.BorrowReturnId == id);
+            if (borrowReturn == null)
+            {
+                return NotFound();
+            }
+
+            return View(borrowReturn);
+        }
+
+        // POST: Return/Edit/{id}]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("return/{id}")]
+        public IActionResult Return(int id, BorrowReturn borrowReturn)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingBorrowReturn = db.BorrowReturns.Find(id);
+                if (existingBorrowReturn != null)
+                {
+                    existingBorrowReturn.ReturnDate = borrowReturn.ReturnDate;
+                    db.SaveChanges();
+                    return RedirectToAction("ListBorrowReturn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Không tìm thấy bản ghi mượn trả.");
+                }
+            }
+
+            return View(borrowReturn);
+        }
+
+    }
+}
