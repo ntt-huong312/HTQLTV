3d9e07a885510137ee29ef649900685db2d1b49c
ntt-huong312 <nguyenthithanhhuong.0652@gmail.com>
2024-06-05 18:37:18 +0700

    update models category

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,7 +5,7 @@ namespace HTQLTV.Models;
 
 public partial class Category
 {
-    public int? CategoryId { get; set; }
+    public int CategoryId { get; set; }
 
     public string CategoryName { get; set; } = null!;
 
