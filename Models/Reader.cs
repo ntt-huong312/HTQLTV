27adcea67a9a0ff2179f4b7df9806fb0e7780fbf
ntt-huong312 <nguyenthithanhhuong.0652@gmail.com>
2024-06-11 21:20:58 +0700

    fix trang chủ + thêm ràng buộc reader

--- Models/Reader.cs
+++ Models/Reader.cs
@@ -10,22 +10,29 @@ public partial class Reader
 
     [Required]
     [Display(Name = "Họ và tên")]
+    [StringLength(50, ErrorMessage = "Họ tên không vượt quá 50 ký tự")]
+    [MinLength(3, ErrorMessage = "Họ tên phải có ít nhất 3 ký tự")]
     public string FullName { get; set; } = null!;
 
     [Required]
     [Display(Name = "Địa chỉ")]
+    [StringLength(150, ErrorMessage = "Địa chỉ không vượt quá 150 ký tự")]
+    [MinLength(10, ErrorMessage = "Địa chỉ có ít nhất 10 ký tự")]
     public string ReaderAddress { get; set; } = null!;
 
     [Required]
     [Display(Name = "Số điện thoại")]
+    [Phone(ErrorMessage = "Số điện thoại không hợp lệ")]
     public string PhoneNumber { get; set; } = null!;
 
-    [Required]
     [Display(Name = "Email")]
+    [Required(ErrorMessage = "Số điện thoại là bắt buộc")]
+    [EmailAddress(ErrorMessage = "Email không hợp lệ")]
     public string Email { get; set; } = null!;
 
-    [Required]
     [Display(Name = "Ngày sinh")]
+    [Required(ErrorMessage = "Vui lòng chọn hạn trả.")]
+    [DataType(DataType.Date)]
     public DateOnly DateOfBirth { get; set; }
 
     public virtual ICollection<BorrowReturn> BorrowReturns { get; set; } = new List<BorrowReturn>();
