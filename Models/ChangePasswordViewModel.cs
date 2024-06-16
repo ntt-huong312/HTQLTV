5b488edadea035c4557b06a87bf56184330e8cd3
DoanPhuJST <phudoan2703@gmail.com>
2024-06-16 12:09:02 +0700

    DoiMK

---
+++ Models/ChangePasswordViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HTQLTV.Models
+{
+    public class ChangePasswordViewModel
+    {
+        public string Email { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    } 
+}
